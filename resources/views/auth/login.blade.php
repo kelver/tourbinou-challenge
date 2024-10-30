77b3f7e79a9e3f859653dc263d54db21d2dd5183
kelver <kelver.machado@coresoft.technology>
2024-10-30 18:20:50 -0400

    chore: Rotas e componentes de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,46 +2,50 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <div class="w-[360px] bg-white p-10 rounded-lg shadow-lg">
+        <div class="w-full flex justify-center mb-6">
+            <a href="/">
+                <x-application-logo class="fill-current text-gray-500" width="200" />
+            </a>
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="max-w-sm space-y-3">
+                <div class="relative">
+                    <label for="email">Usuário</label>
+                    <input type="email" name="email" id="email"
+                        class="peer py-3 px-4 ps-2 block w-full bg-gray-100 border-transparent
+                        rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50
+                        disabled:pointer-events-none" placeholder="usuario@email.com">
+                    <div class="absolute inset-y-0 end-0 flex items-center pointer-events-none pe-4 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
+                        <svg class="shrink-0 size-4 text-gray-500 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                            <circle cx="12" cy="7" r="4"></circle>
+                        </svg>
+                    </div>
+                </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+                <div class="relative">
+                    <label for="password">Senha</label>
+                    <input type="password" name="password" id="password"
+                        class="peer py-3 px-4 ps-2 block w-full bg-gray-100 border-transparent
+                        rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50
+                        disabled:pointer-events-none" placeholder="********">
+                    <div class="absolute inset-y-0 end-0 flex items-center pointer-events-none pe-4 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
+                        <svg class="shrink-0 size-4 text-gray-500 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M2 18v3c0 .6.4 1 1 1h4v-3h3v-3h2l1.4-1.4a6.5 6.5 0 1 0-4-4Z"></path>
+                            <circle cx="16.5" cy="7.5" r=".5"></circle>
+                        </svg>
+                    </div>
+                </div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+                <div class="w-full flex relative justify-center">
+                    <button type="submit" class="w-1/3 mt-3 py-3 bg-orange-500 text-white font-semibold rounded-md hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500">
+                        Login
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
 </x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/destinations', App\Livewire\Destinations\Index::class)->name('destinations.index');
+    Route::get('/destinations/{id?}', App\Livewire\Destinations\CreateEdit::class)->name('destinations.create-edit');
+
+    Route::get('/trips', App\Livewire\Trips\Index::class)->name('trips.index');
+    Route::get('/trips/{id?}', App\Livewire\Trips\CreateEdit::class)->name('trips.create-edit');
 });
 
 require __DIR__.'/auth.php';
