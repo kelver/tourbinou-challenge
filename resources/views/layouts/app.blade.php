957cd788aed7a1d759ff3f1ad57164d48da17bd5
kelver <kelver.machado@coresoft.technology>
2024-11-01 13:15:40 -0400

    chore: Adição de script no carregamento da aplicação.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,9 @@
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
         <!-- Scripts -->
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <script src="{{ asset('assets/js/stateandcity.js') }}"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="{{ asset('assets/js/destinations.js') }}"></script>
+        <script src="{{ asset('assets/js/stateandcity.js') }}"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
@@ -36,7 +37,6 @@
             </main>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-        <script src="{{ asset('assets/js/sweetAlert.js') }}"></script>
 
         <script>
             document.addEventListener('DOMContentLoaded', function () {
