ac1b94f90b6a2b3e14839e8e1d2774de0dc0c29f
kelver <kelver.machado@coresoft.technology>
2024-11-01 16:25:09 -0400

    style: Feedback no front end para validação de obrigatoriedade.

--- resources/views/trips/form.blade.php
+++ resources/views/trips/form.blade.php
@@ -48,7 +48,7 @@
                 <div class="w-full flex flex-col col-span-3">
                     <label for="description">Descrição</label>
 
-                    <textarea id="description" name="description"
+                    <textarea id="description" name="description" maxlength="260"
                         class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500
                         focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900
                         dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500
